cec72a432d9b9f68067895182da62851c1aa88d2
RS-Adi <srepudi@uwm.edu>
2025-03-01 14:57:08 -0800

    Updated the js file added necessary errors and atuhentication methods, lots of spelling mistakes

--- Readme.md
+++ Readme.md
@@ -3,4 +3,3 @@ This logs all thw work i did and the ones im going to do.
 /*First steps*/
 
 Created the index styles and app files for the initial process.
-
--- app.js
+++ app.js
@@ -4,4 +4,27 @@ document.getElementById('login-form').addEventListener('submit', async(e) =>{
     //First get theuser input
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-})
+
+    //Lets make the API call to authenticate
+    try{
+        const response = await fetch('https://frontend-take-home-service.fetch.com/auth/login',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({name, email}),
+                credentials: 'include', //This includes cookies
+            });
+            if (response.ok) {
+                //Takes to search page after it is successfull
+                window.location.href = 'search.html'; 
+            }else{
+                //Display error
+                document.getElementById('error-message').textContent = 'Login failed. Please check your email.';
+            }
+    }catch(error){
+        console.error('Error during login:', error);
+        document.getElementById('error-message').textContent = 'An error occured. Please try again after checking your details'
+    }
+});
