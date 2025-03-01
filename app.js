d0ff43d4f2b7f401b5685b36270c6bdf2e07e0b4
RS-Adi <srepudi@uwm.edu>
2025-03-01 14:41:46 -0800

    Update the index and style files added necessary stylings for the website

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('login-form').addEventListener('submit', async(e) =>{
+    e.preventDefault(); //This prevent the form from submitting in traditional ways
+
+    //First get theuser input
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+})
